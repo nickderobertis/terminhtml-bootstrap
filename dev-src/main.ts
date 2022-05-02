d7faaa877c2de4857b4bd1145e4ebd0eb06ed2b9
Nick DeRobertis <whoopnip@gmail.com>
2022-05-02 17:26:49 -0400

    chore: Add development app

--- dev-src/main.ts
+++ dev-src/main.ts
@@ -1,8 +1,3 @@
 import './style.css';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>Hello bootstrap</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+// Importing the library should do the bootstrapping
+import '../src';
