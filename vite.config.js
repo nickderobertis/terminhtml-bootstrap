8e555f8f2da1273fb43e2cce2bddcc54aa6e9032
Nick DeRobertis <whoopnip@gmail.com>
2022-05-05 14:54:29 -0400

    Update flexlate templates
    copier-typescript-npm-sphinx: 1e8bd4374603015620f378a948db1b6066d8b2a6
    
    -------------------BEGIN FLEXLATE TRANSACTION-------------------
    {
      "type": "update",
      "target": "",
      "out_root": null,
      "data": null,
      "id": "4130d850-6cd4-4a35-91a9-0798f12faa26"
    }

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+const path = require("path")
+const { defineConfig } = require("vite")
 
 module.exports = defineConfig({
-  root: path.resolve(__dirname, './dev-src'),
+  root: path.resolve(__dirname, "./dev-src"),
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: '@terminhtml/bootstrap',
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@terminhtml/bootstrap",
       fileName: (format) => `@terminhtml-bootstrap.${format}.js`
     },
-    outDir: path.resolve(__dirname, './dist'),
+    outDir: path.resolve(__dirname, "./dist"),
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
+      // make sure to externalize deps that shouldn"t be bundled
       // into your library
       external: [],
       output: {
@@ -19,16 +19,17 @@ module.exports = defineConfig({
         // for externalized deps
         globals: {}
       }
-    }
+    },
+    sourcemap: true,
   },
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
     include: [
       path.resolve(
         __dirname,
-        './test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
+        "./test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
       ),
     ],
   },
