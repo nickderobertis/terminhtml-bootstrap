b5245652ce5d1605188796c2d9b7c4e100a8a564
Nick DeRobertis <whoopnip@gmail.com>
2022-05-18 06:16:52 -0400

    fix: Add terminhtml to non-bundle dependencies, allows build to work with optional dynamic es import

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn"t be bundled
       // into your library
-      external: [],
+      external: ["terminhtml"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
